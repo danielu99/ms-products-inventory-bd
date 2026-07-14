a283ffeba5ab336951c9317cc479cf1ff703bbaa
Daniel León <danielulc@outlook.com>
2026-07-14 00:39:13 -0600

    Se modifica CorsPolicy

--- src/main/java/com/elmundoexterior/ms_products_inventory_bd/shared/config/CorsConfig.java
+++ src/main/java/com/elmundoexterior/ms_products_inventory_bd/shared/config/CorsConfig.java
@@ -20,7 +20,8 @@ public class CorsConfig {
                 registry
                         .addMapping("/**")
                         .allowedOrigins(
-                                "http://localhost:5173")
+                                "http://localhost:5173",
+                "https://elmundoexterior-mvp.vercel.app")
                         .allowedMethods("*");
             }
         };
